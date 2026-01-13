a0f922498c0dc3f574c2017c93f3ea3a7f04863b
Seun <seunfagade@gmail.com>
2026-01-13 10:40:53 +0000

    commit husky

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  arrowParens: 'avoid',
-  singleQuote: true,
-  trailingComma: 'all',
+	arrowParens: 'always',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	semi: true,
+	printWidth: 100,
+	useTabs: true,
+	tabWidth: 4,
+	arrowParens: 'always',
+	endOfLine: 'lf',
+	trailingComma: 'all',
 };
--- App.tsx
+++ App.tsx
@@ -6,40 +6,41 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import { JSX, useEffect } from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): JSX.Element {
+	const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+	useEffect(() => {
+		doNothing();
+	}, []);
+
+	const doNothing = () => {};
+
+	return (
+		<SafeAreaProvider>
+			<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+			<AppContent />
+		</SafeAreaProvider>
+	);
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+	const safeAreaInsets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<NewAppScreen templateFileName='App.tsx' safeAreaInsets={safeAreaInsets} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
 
 export default App;
